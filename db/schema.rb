429ff96d31b51fc765acfc12babd49eb9941a5b9
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-12-05 15:55:39 +0100

    suppression et migration des columns inutiles dans user + ajout de la colonne admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205121553) do
+ActiveRecord::Schema.define(version: 20171205145344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,10 +95,7 @@ ActiveRecord::Schema.define(version: 20171205121553) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "phone_number"
-    t.date "birthday"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
