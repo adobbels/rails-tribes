eecc602c5e566b9569444ecd0bf0592688766f28
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-12-05 15:57:24 +0100

    suppression de la colonne category dans la table profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205145344) do
+ActiveRecord::Schema.define(version: 20171205145604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 20171205145344) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.string "category"
     t.string "address"
     t.integer "post_code"
     t.string "city"
