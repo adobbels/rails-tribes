4576fcd133660f345d686a0f4dd5b0d96f318403
Aurelien Dobbels <adobbels@gmail.com>
2017-12-06 16:20:02 +0100

    gmap fixed with Clem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,6 @@
-console.log('Hello World from toto')
-
 //= require rails-ujs
-// require ./cable
 //= require jquery
 //= require bootstrap-sprockets
-
 //= require underscore
 //= require gmaps/google
 
