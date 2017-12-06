af33a92289fbf1a3384e4e71b2f9b892928e5da4
Aurelien Dobbels <adobbels@gmail.com>
2017-12-06 10:47:15 +0100

    model updated and migration created

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,4 +5,12 @@ class House < ApplicationRecord
   has_many :features, through: :house_options
 
   mount_uploader :photos, PhotoUploader
+
+  geocoded_by :full_adress
+  after_validation :geocode, if: :address_changed?
+
+
+  def full_adress
+    "#{address} #{post_code} #{city} #{country}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205150650) do
+ActiveRecord::Schema.define(version: 20171206094528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20171205150650) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "profiles", force: :cascade do |t|
