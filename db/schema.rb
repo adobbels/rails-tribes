9c61b6db178b2681957efb3a1d863911f51889bf
Aurelien Dobbels <adobbels@gmail.com>
2017-12-08 13:48:52 +0100

    migration add price with add_monetize

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208124052) do
+ActiveRecord::Schema.define(version: 20171208124703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20171208124052) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "price_cents", default: 0, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
