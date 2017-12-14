ff14d4f6483d546f4253341b29a374dbae7bae27
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-12-14 15:00:30 +0100

    on peut supprimer les profils depuis la console

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
   has_attachment :photo
 
   belongs_to :user
-  has_many :reviews
-  has_many :bookings
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
 end
